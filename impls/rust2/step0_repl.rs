af0ade87169f8f963b1f0a90c5b86c02de90344e
ryo-imai-bit <mayoimaimai9@gmail.com>
2024-02-26 19:57:06 +0900

    update README and impl step0

--- impls/rust2/step0_repl.rs
+++ impls/rust2/step0_repl.rs
@@ -1,3 +1,16 @@
+use std::io::{Write, stdin, stdout};
+
 fn main() {
-    println!("Hello, world!")
+    loop {
+        print!("user> ");
+        stdout().flush().unwrap();
+        let mut line = String::new();
+        match stdin().read_line(&mut line) {
+            Ok(0) => return,
+            Ok(_) => (),
+            Err(_) => println!("Error reading line")
+        }
+
+        println!("{}", line)
+    }
 }
